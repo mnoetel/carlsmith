be48bb0385c268c003750ba9d16cb75395bd0774
mnoetel <noetel@gmail.com>
2022-11-16 13:59:59 +1000

    Updating probabilities for carlsmith_gg_sankey_shiny

--- carlsmith_gg_sankey_shiny.R
+++ carlsmith_gg_sankey_shiny.R
@@ -9,16 +9,16 @@ ui <- shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      h5("For each question, answer on a scale from 0 = impossible to 1 = certain."),
-      sliderInput("feasible", "Will Advanced, Planning, and Strategically-aware systems be feasible?",
+      h5("From 0 = impossible to 1 = certain, what do you think is the probility that..."),
+      sliderInput("feasible", "Advanced, Planning, and Strategically-aware systems will be feasible?",
                   value = 0.65, min = 0.01, max = 1, step = .01),
-      sliderInput("incentives", "Will we want to make them?",
+      sliderInput("incentives", "We will make them, despite the risks?",
                   value = 0.8, min = 0.01, max = 1, step = .01),
-      sliderInput("misaligned", "If we make them, would they be hard to align?",
+      sliderInput("misaligned", "If we make them, they would be hard to align?",
                   value = 0.4, min = 0.01, max = 1, step = .01),
-      sliderInput("powerseeking", "If misaligned, would they seek power to do a lot of damage?",
+      sliderInput("powerseeking", "If misaligned, they would do a lot of damage by seeking power?",
                   value = 0.65, min = 0.01, max = 1, step = .01),
-      sliderInput("impact", "If they do a lot of damage, could they disempower us?",
+      sliderInput("impact", "If they do a lot of damage, they could disempower us?",
                   value = 0.4, min = 0.01, max = 1, step = .01),
       width = 3),
     mainPanel(
@@ -27,7 +27,7 @@ ui <- shinyUI(fluidPage(
                                  font-style: italic;
                                  }"
       )),
-      sankeyNetworkOutput('plot', width = "1400px", height = "600px"),
+      sankeyNetworkOutput('plot'),#, width = "1400px", height = "600px"),
       textOutput('result_as_text'),
       tags$head(tags$style("#result_as_text{font-size: 16px;
                                  font-style: italic;
@@ -67,8 +67,12 @@ server <- function(input, output) {
     x[, 4] <- as.factor(x[, 4])
     return(x)
   })
-  p_as_table <- data.frame(prob = as.numeric(c(1/7, 1/11, 1/31, 1/51, 1/101, 1/1001, 1/10001, 1/1000001, 1/1000000001)),
-                           what = as.character(c("chance of death for a pedestrian hit by a car traveling at 45 kph",
+  p_as_table <- data.frame(prob = as.numeric(c(.9, .85, 1/2, 1/6, 1/7, 1/11, 1/31, 1/51, 1/101, 1/1001, 1/10001, 1/1000001, 1/1000000001)),
+                           what = as.character(c("chance you're not really estimating existental risk, but are trying to be cheeky and see how high these statements can go",
+                                                 "chance of a US citizen finishing high school",
+                                                 "chance of flipped coin landing heads",
+                                                 "chance of death in russian roulette",
+                                                 "chance of death for a pedestrian hit by a car traveling at 45 kph",
                                                  "chance that a smoker’s habit will kill them before age 60",
                                                  "chance that having unprotected sex once will lead to pregnancy",
                                                  "chance of death for a US solider serving in the Vietnam War for one year",
@@ -107,7 +111,7 @@ server <- function(input, output) {
     sankeyNetwork(Links = links(),
                   Nodes = nodes, Source = "source",
                   Target = "target", Value = "value", NodeID = "name",
-                  units = "p", fontSize = 14, nodeWidth = 15,
+                  fontSize = 14, nodeWidth = 15,
                   colourScale = my_color, LinkGroup="group", NodeGroup="group",
                   nodePadding = 20, sinksRight = FALSE)
   })
